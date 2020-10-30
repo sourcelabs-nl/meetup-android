1ec5c201265908f930fff1469241cb05debcac84
danielmast <danielmast92@gmail.com>
2020-10-30 15:39:13 +0100

    Get Rick and Morty characters with retrofit

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.myfirstapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
@@ -20,7 +28,6 @@ class MainActivity : AppCompatActivity() {
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java)
                 .apply { putExtra(EXTRA_MESSAGE, message) }
-
         startActivity(intent)
     }
 }
